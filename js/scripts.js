7a87ba3ee25ca332772f169daba22cf60d473a42
Shilpa Agrawal <shilpa.mandloi@gmail.com>
2013-06-17 15:58:35 +0530

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -6,41 +6,47 @@ $(document).ready(function(){
 
 	$('#ProjectSlides .AllProjects:gt(0)').hide();
 
-	$("#proj1img1a").hide();
-	$("#proj1img1b").hide();
-	$("#proj1img1c").hide();
-	$("#proj2img2a").hide();
-	$("#proj2img2b").hide();
-	$("#proj3img3a").hide();
-	$("#proj3img3b").hide();
-	$("#proj4img4a").hide();
-	$("#proj4img4b").hide();
+	$('#submenu-item').hide();
+
+	// $("#proj1img1a").hide();
+	// $("#proj1img1b").hide();
+	// $("#proj1img1c").hide();
+	// $("#proj2img2a").hide();
+	// $("#proj2img2b").hide();
+	// $("#proj3img3a").hide();
+	// $("#proj3img3b").hide();
+	// $("#proj4img4a").hide();
+	// $("#proj4img4b").hide();
 	
 	// $.colorbox.settings.returnFocus = "false";
-	$.colorbox.settings.width = "600px";
+	$.colorbox.settings.width = "700px";
 	$.colorbox.settings.height = "600px";
 	$.colorbox.settings.opacity="0.8";
 	$.colorbox.settings.scalePhotos="true";
 	$.colorbox.settings.slideshow='true';
 
-	$(".proj2img").colorbox({rel:'group2'});
+	
 	$(".proj1img").colorbox({rel:'group1'});
+	$(".proj2img").colorbox({rel:'group2'});
 	$(".proj3img").colorbox({rel:'group3'});
 	$(".proj4img").colorbox({rel:'group4'});
-	
+	$(".proj5img").colorbox({rel:'group5'});
+	$(".proj6img").colorbox({rel:'group6'});
 
 	// On refresh of the page highlight correct menu option
 	//select_menu();
 
-	$("#menu li").click(function() {
+	$("ul.topmenu > li").click(function() {
 
-		str = $(this).text().replace(/\s/g, '');
-		scrollToSection('#'+str);
+		// str = $(this).text().replace(/\s/g, '');
+		str = $(this).attr("id");
+		//alert(str);
+		scrollToSection('#'+str, "#" + str+"Section");
 	});
 
 
 	$("#logo-image").click(function() {
-		scrollToSection('#Home');
+		scrollToSection('#Home', "#HomeSection");
 	});
 
 
@@ -59,34 +65,43 @@ $(document).ready(function(){
 });
 
 
-function scrollToSection(section) {
+function scrollToSection(sectionMenu, section) {
 	if ($('body').scrollTop() != $(section).offset().top) 
 	{
-		$('#menu li').removeClass("menu-item-selected");
-		$('#menu li').addClass("menu-item");
-		$(section + '-menu-item').removeClass("menu-item");
-		$(section + '-menu-item').addClass("menu-item-selected");
+		//alert(section);
+		$('ul.topmenu > li').removeClass("menu-item-selected");
+		$('ul.topmenu > li').addClass("menu-item");
+		$(sectionMenu ).removeClass("menu-item");
+		$(sectionMenu ).addClass("menu-item-selected");
 		$('body').animate({scrollTop: $(section).offset().top}, 'slow');
 	}
 }
 
 function select_menu(){
-	highlightMenu('#Home');
-	highlightMenu('#Projects');
-	highlightMenu('#About');
-	highlightMenu('#Services');
-	highlightMenu('#GetInTouch');
+	highlightMenu('#HomeSection','#ProjectsSection');
+	highlightMenu('#ProjectsSection','#AboutSection');
+	highlightMenu('#AboutSection','#ServicesSection');
+	highlightMenu('#ServicesSection','#GetInTouchSection');
+	highlightMenu('#GetInTouchSection');
 }
 
-function highlightMenu (section) {
-	if($('body').scrollTop() == $(section).offset().top)
+function highlightMenu (section, nextSection) {
+	if($('body').scrollTop() >= $(section).offset().top && $('body').scrollTop() < $(nextSection).offset().top )
 	{
 		//alert(section);
-		$(section + '-menu-item').addClass("menu-item-selected");
+		$(section).addClass("menu-item-selected");
 	}
 	else {
-		$(section + '-menu-item').addClass("menu-item");	
+		$(section).addClass("menu-item");	
 	}
 }
 
+
+function showSubMenu (){
+	$('.submenu-list').show('fast');			
+}
+
+function hideSubMenu(){
+	$('.submenu-list').hide('fast');
+}
 
