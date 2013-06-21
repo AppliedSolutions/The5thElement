883f2bf00b4d7b29ef571f0bda04be15ed4c9623
Shilpa Agrawal <shilpa.mandloi@gmail.com>
2013-06-21 16:09:27 +0530

    With Project Section and background images

--- js/scripts.js
+++ js/scripts.js
@@ -4,76 +4,104 @@ $(document).ready(function(){
 
 	$('#HomeContents').animate({'marginLeft' : '-=10px', 'top':'+=10px', 'opacity':'1'},500);
 
-	$('#ProjectSlides .AllProjects:gt(0)').hide();
-
 	$('#submenu-item').hide();
 
-	// $("#proj1img1a").hide();
-	// $("#proj1img1b").hide();
-	// $("#proj1img1c").hide();
-	// $("#proj2img2a").hide();
-	// $("#proj2img2b").hide();
-	// $("#proj3img3a").hide();
-	// $("#proj3img3b").hide();
-	// $("#proj4img4a").hide();
-	// $("#proj4img4b").hide();
-	
-	// $.colorbox.settings.returnFocus = "false";
 	$.colorbox.settings.width = "700px";
-	$.colorbox.settings.height = "600px";
-	$.colorbox.settings.opacity="0.8";
+	$.colorbox.settings.height = "500px";
+	// $.colorbox.settings.opacity="0.8";
 	$.colorbox.settings.scalePhotos="true";
 	$.colorbox.settings.slideshow='true';
+	$.colorbox.settings.returnFocus='false';
+	$.colorbox.settings.trapFocus='false';
+	// $.colorbox({
+	// 	onComplete : function() {
+	// 		alert("Complete!");
+	// 		$('.slider').animate({ left: $('.wrapper').attr('left') });	
+	// 	}
+	// });
 
+
+
+	$('.LivingSpaces1').colorbox({rel:'group1'});
+	$('.LivingSpaces2').colorbox({rel:'group2'});
+	$('.LivingSpaces3').colorbox({rel:'group3'});
+	$('.LivingSpaces4').colorbox({rel:'group4'});
+	$('.LivingSpaces5').colorbox({rel:'group5'});
+	$('.LivingSpaces6').colorbox({rel:'group6'});
+	$('.LivingSpaces7').colorbox({rel:'group7'});
+	$('.LivingSpaces8').colorbox({rel:'group8'});
+	$('.LivingSpaces9').colorbox({rel:'group9'});
+	$('.outdoors1').colorbox({rel:'group10'});
+	$('.outdoors2').colorbox({rel:'group11'});
+	$('.outdoors3').colorbox({rel:'group12'});
 	
-	$(".proj1img").colorbox({rel:'group1'});
-	$(".proj2img").colorbox({rel:'group2'});
-	$(".proj3img").colorbox({rel:'group3'});
-	$(".proj4img").colorbox({rel:'group4'});
-	$(".proj5img").colorbox({rel:'group5'});
-	$(".proj6img").colorbox({rel:'group6'});
+	$('.worksocial1').colorbox({rel:'group13'});
+	$('.worksocial2').colorbox({rel:'group14'});
+	$('.worksocial3').colorbox({rel:'group15'});
 
 	// On refresh of the page highlight correct menu option
 	//select_menu();
 
-	$("ul.topmenu > li").click(function() {
-
-		// str = $(this).text().replace(/\s/g, '');
+	$("ul.topmenu > li").click(function(event) {
+		event.preventDefault();
 		str = $(this).attr("id");
-		//alert(str);
 		scrollToSection('#'+str, "#" + str+"Section");
 	});
 
+	$("ul.submenu-list > li").click(function() {
+
+		str = $(this).attr("id");
+		scrollToSubSection('#'+str, "#" + str+"Section");
+	});
 
 	$("#logo-image").click(function() {
 		scrollToSection('#Home', "#HomeSection");
 	});
 
 
-	$('.NextProject').click(function() {
-	    //$('#ProjectSlides .AllProjects:first-child').fadeOut().next().fadeIn().end().appendTo('#ProjectSlides');
-	    $('#ProjectSlides .AllProjects:first-child').fadeOut();
-	    $('#ProjectSlides .AllProjects:first-child').next().fadeIn();
-	    $('#ProjectSlides .AllProjects:first-child').appendTo('#ProjectSlides');
-	});
+	// $('.NextProject').click(function() {
+	//     //$('#ProjectSlides .AllProjects:first-child').fadeOut().next().fadeIn().end().appendTo('#ProjectSlides');
+	//     $('#ProjectSlides .AllProjects:first-child').fadeOut();
+	//     $('#ProjectSlides .AllProjects:first-child').next().fadeIn();
+	//     $('#ProjectSlides .AllProjects:first-child').appendTo('#ProjectSlides');
+	// });
 
-	$('.PrevProject').click(function() {
-	    $('#ProjectSlides .AllProjects:first-child').fadeOut();
-	    $('#ProjectSlides .AllProjects:last-child').prependTo('#ProjectSlides');//.fadeOut();
-	    $('#ProjectSlides .AllProjects:first-child').fadeIn();
-	});
+	// $('.PrevProject').click(function() {
+	//     $('#ProjectSlides .AllProjects:first-child').fadeOut();
+	//     $('#ProjectSlides .AllProjects:last-child').prependTo('#ProjectSlides');//.fadeOut();
+	//     $('#ProjectSlides .AllProjects:first-child').fadeIn();
+	// });
 });
 
 
 function scrollToSection(sectionMenu, section) {
-	if ($('body').scrollTop() != $(section).offset().top) 
+	if ($('body, html').scrollTop() != $(section).offset().top) 
 	{
-		//alert(section);
 		$('ul.topmenu > li').removeClass("menu-item-selected");
 		$('ul.topmenu > li').addClass("menu-item");
 		$(sectionMenu ).removeClass("menu-item");
 		$(sectionMenu ).addClass("menu-item-selected");
-		$('body').animate({scrollTop: $(section).offset().top}, 'slow');
+		$('body, html').animate({scrollTop: $(section).offset().top}, 'slow');
+		// if(sectionMenu != "#Projects")
+		// {
+		// 	$('body').animate({scrollTop: $(section).offset().top}, 'slow');
+		// }
+		// else
+		// {
+		// 	$('body').animate({scrollTop: $("#LivingSpacesSection").offset().top}, 'slow');
+		// 	$("#LivingSpaces" ).addClass("submenu-item-selected");	
+		// }
+	}
+}
+
+function scrollToSubSection(sectionMenu, section) {
+	if ($('body, html').scrollTop() != $(section).offset().top) 
+	{
+		$('ul.submenu-list > li').removeClass("submenu-item-selected");
+		$('ul.submenu-list > li').addClass("submenu-item");
+		$(sectionMenu ).removeClass("submenu-item");
+		$(sectionMenu ).addClass("submenu-item-selected");
+		$('body, html').animate({scrollTop: $(section).offset().top}, 'slow');
 	}
 }
 
@@ -86,9 +114,8 @@ function select_menu(){
 }
 
 function highlightMenu (section, nextSection) {
-	if($('body').scrollTop() >= $(section).offset().top && $('body').scrollTop() < $(nextSection).offset().top )
+	if($('body, html').scrollTop() >= $(section).offset().top && $('body, html').scrollTop() < $(nextSection).offset().top )
 	{
-		//alert(section);
 		$(section).addClass("menu-item-selected");
 	}
 	else {
@@ -102,6 +129,18 @@ function showSubMenu (){
 }
 
 function hideSubMenu(){
+	$('ul.submenu-list > li').removeClass("submenu-item-selected");
+	$('ul.submenu-list > li').addClass("submenu-item");
 	$('.submenu-list').hide('fast');
 }
 
+
+
+
+$(document).bind('cbox_closed', function(){ 
+	// alert("Closed"); 
+
+// 	$('.slider').animate({ left: $('.wrapper').attr('left') });	 
+	var elementStyle = $.colorbox.element().style;
+	elementStyle.position = "relative";
+});
